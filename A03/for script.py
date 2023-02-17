26b40f4b8e5fdbddd891d7f69df746fdf5a3fcef
nranabhat <78765060+nranabhat@users.noreply.github.com>
2023-02-16 20:36:03 -0600

    my first commit!

---
+++ A03/for script.py
@@ -0,0 +1,5 @@
+counter = 10
+for i in range(10):
+    print(counter)
+    counter = counter - 1
+print('\n')
---
+++ A03/if script.py
@@ -0,0 +1,5 @@
+n = int(input("Enter a value: "))
+if n < 10:
+    print("It is a one digit number")
+else:
+    print("It is a two digit number")
