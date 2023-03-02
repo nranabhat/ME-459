defbce3bd21a753e9c319d7c646d883cdaa340e8
nranabhat <78765060+nranabhat@users.noreply.github.com>
2023-03-01 22:41:30 -0600

    with example

--- A05/task4.py
+++ A05/task4.py
@@ -7,8 +7,10 @@ class ComplexNumber:
         return (self.real ** 2 + self.imag ** 2) ** 0.5
 
     def __format__(self, format_spec):
-        return '{}{}j'.format(format(self.real, format_spec), 
+        return '{}+{}j'.format(format(self.real, format_spec), 
                               format(self.imag, format_spec))
     
 
-    
+c = ComplexNumber(3, 4)
+print(abs(c))
+print(format(c, '.2f'))
